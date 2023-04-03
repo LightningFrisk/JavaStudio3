6f4714f0f3e4d5dc25008cd49f961bd5d54de0ea
LightningFrisk <bmxcaleb@gmail.com>
2023-04-03 18:47:59 -0500

    creating

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+package org.launchcode.java.studios.restaurantmenu;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+//Menu
+//  Date updated – Date class
+//  Items – ArrayList of MenuItems
+//      Needs to hold all the MenuItems
+//      Could split this into multiple fields (appetizers, entrée’s, deserts, etc)
+//  mostRecent – Maybe just select the most recent item pushed into array list
+//      Has the item most recently added
+//      optional
+//MenuItem
+//  Price – double
+//  Description – string
+//  Category – string
+//  New – Boolean
+//  Name – String
+//  isAvailable – Boolean
+//  isVegan - Boolean
+//  dateAdded – Date class
+//  Allergens – ArrayList of strings
+//  spiceLevel – Int
+//  proteinType - string
